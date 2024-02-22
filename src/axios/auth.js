da99609d95ce4bc4d366481aa82808ccc6efe176
KY <rudyeon@naver.com>
2024-02-22 15:20:33 +0900

    2-1. SignupPage.jsx

--- src/axios/auth.js
+++ src/axios/auth.js
@@ -1,5 +1,21 @@
 import axios from "axios";
 
-export const authApi = axios.create({});
+export const authApi = axios.create({
+  baseURL: "https://moneyfulpublicpolicy.co.kr",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-authApi.interceptors.request.use();
+authApi.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer${accessToken}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
