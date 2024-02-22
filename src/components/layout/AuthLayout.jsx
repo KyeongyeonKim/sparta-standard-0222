20ecae1c8fc75d89624cb1de06cafa9d8438d36f
KY <rudyeon@naver.com>
2024-02-22 14:28:15 +0900

    1. Router 작성

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -8,7 +8,9 @@ const AuthLayout = () => {
       <h1>Auth Layout</h1>
       <p>반드시 로그인이 되어있어야 하는 페이지입니다.</p>
       <Navigation />
-      <main></main>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
